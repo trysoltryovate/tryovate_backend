42e69cc6b5c7b7784059aecfc8b85163ae08648f
mohansai <mohansai75872@gmail.com>
2025-04-22 18:14:34 +0530

    changes in configure file

--- src/main/java/com/tryovate/configure/WebConfigure.java
+++ src/main/java/com/tryovate/configure/WebConfigure.java
@@ -11,7 +11,7 @@ public class WebConfigure implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // your React app URL
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
